a937e7a4f0a3b05573929e3f8f6f3a3bc7628ba0
stephbryson98 <sbryson@students.stonehill.edu>
2019-12-13 10:50:38 -0500

    add sccript

--- R/sdmfinal2.R
+++ R/sdmfinal2.R
@@ -50,7 +50,7 @@ preds = wc[[predvars]]
 preds85 = wc_future85[[predvars]]
 
 # crop to study area
-ext = extent(40, 110, -10, 50) 
+ext = extent(-90, -10, -70, 20) 
 preds = crop(preds, ext)
 preds85 = crop(preds85, ext)
 
